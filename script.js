0dd0f2d9acc214a57192c5b89e07c4eff56c3018
Gunjan Rajput <116620443+Gunjan-Rajput@users.noreply.github.com>
2024-05-07 11:34:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const getStartedBtn = document.getElementById('getStartedBtn');
+    
+    getStartedBtn.addEventListener('click', function() {
+        // You can add functionality here, such as redirecting to another page or showing a modal
+        alert('Button clicked!');
+    });
+});
